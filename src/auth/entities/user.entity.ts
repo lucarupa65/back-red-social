4989c538a6b85bcd3e69cdbe4a91d279aa1f8d74
Luis Carlos <lucarupa65@gmail.com>
2024-02-03 11:07:36 -0500

    Conexion con base de datos y primera entida

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,39 @@
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+
+@Entity('users')
+export class User {
+    
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text', {
+        unique: true
+    })
+    email: string;
+
+    @Column('text', {
+        select: false
+    })
+    password: string;
+
+    @Column('text')
+    fullName: string;
+
+    @Column('int', {
+        default: 0
+    })
+    Age: number;
+
+
+    @BeforeInsert()
+    checkFieldsBeforeInsert() {
+        this.email = this.email.toLowerCase().trim();
+    }
+
+    @BeforeUpdate()
+    checkFieldsBeforeUpdate() {
+        this.checkFieldsBeforeInsert();   
+    }
+
+}
