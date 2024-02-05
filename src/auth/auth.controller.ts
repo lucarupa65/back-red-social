0a0c29fb994c0d824f9a6b4b795c425310d8ee00
Luis Carlos <lucarupa65@gmail.com>
2024-02-04 19:36:05 -0500

    Docmentacion Auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,16 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-} from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 import { AuthService } from './auth.service';
-import { Auth, GetUser } from './decorators'
-
+import { Auth, GetUser } from './decorators';
 
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
@@ -18,20 +20,28 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @ApiCreatedResponse({ description: 'Usuario creado' })
+  @ApiBadRequestResponse({
+    description: 'EL correo ya existen o algon dato falta',
+  })
   create(@Body() createAuthDto: CreateUserDto) {
     return this.authService.create(createAuthDto);
   }
 
   @Post('login')
+  @ApiOkResponse({ description: 'Inicio de seccion' })
+  @ApiBadRequestResponse({ description: 'algon dato falta' })
+  @ApiUnauthorizedResponse({ description: 'Las credenciales no son válidas' })
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
   }
 
   @Get('check-status')
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'Token Renovado' })
+  @ApiForbiddenResponse({ description: 'Forbidden. Token invalido' })
   @Auth()
-  checkAuthStatus(
-    @GetUser() user: User
-  ) {
-    return this.authService.checkAuthStatus( user );
+  checkAuthStatus(@GetUser() user: User) {
+    return this.authService.checkAuthStatus(user);
   }
 }
